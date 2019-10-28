029ec62e1ace3e643a34c4b4872333902c89f934
Rino Alfian <rino@bluelogic.ae>
2019-10-28 15:52:50 +0400

    get bot data

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-from flask_pymongo import PyMongo
 from pymongo import MongoClient
+from bson import json_util
 import json
 app = Flask(__name__)
 CORS(app) # very important!
@@ -18,10 +18,24 @@ until here
 ##############################################
 
 
-# @app.route('/avivo', methods=['GET'])
-# def get_all_avivo():
-#   avivo = db.avivo.find()
-#   return jsonify(json.dumps(avivo))
+def get_collection_names(bot_name):
+    collections = json.loads(json_util.dumps(db.list_collection_names()))
+    if bot_name in collections:
+        return 200
+    else:
+        return 500
+
+
+@app.route('/bot-detail', methods=['POST'])
+def bot_data():
+    bot_name = request.json['bot_name']
+    check_bot = get_collection_names(bot_name)
+    if check_bot == 200:
+        bot_load = json.loads(json_util.dumps(db.get_collection(bot_name).find()))
+        print(bot_load)
+        return jsonify(bot_load)
+    else:
+        return jsonify(message="Please Contact Administrator", status=500)
 
 
 if __name__ == '__main__':
