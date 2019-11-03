ea0b68b8626512a5456ca7a653211ecc7902c403
Rino Alfian <rino@bluelogic.ae>
2019-11-03 12:32:43 +0400

    req

--- worker.py
+++ worker.py
@@ -17,7 +17,7 @@ def generate_csv_bot(collection):
             for key in result['results']:
                 csv_file.write("%s,%s\n" % (key['_id'], key['value']))
 
-        send_mail('anton@blelogic.ae', "Attached Report Weekly",collection)
+        send_mail('anton@bluelogic.ae', "Attached Report Weekly",collection)
         return print(result)
     except ValueError as e:
         return print(e)
